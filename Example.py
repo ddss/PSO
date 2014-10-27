2e96a160579725cceb8a9fea84295801ce711ac8
Daniel <ddssantana89@gmail.com>
2014-10-27 11:01:19 -0200

    Detalhamento do exemplo
    Detalhamento do exemplo: 
    - inclusão de comentários descrevendo as linhas de código;
    - modificação do método de resolução para PSO-WL (vide README);
    - inclusão dos símbolos em formato LATEX para criação dos gráficos de performance;
    - inclusão do comando (FO2a2=True) para criação do gráfico da função objetivo em 2d e 3d;
    - comentado a linha para criação de frames (para criá-los, basta retirar da forma de comentário)

--- Example.py
+++ Example.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Example of main program to use with PSO
+Example of main program to be used with PSO
 @author: Daniel
 """
+from PSO import PSO # Importing the PSO class
+from Modelo_benchmark import Modelo # Importing the function to be minimized
 
-from PSO import PSO
-from Modelo_benchmark import Modelo
+sup = [32,32] # maximum value for the parameters
+inf = [-32,-32] # minimum value for the parameters
 
-sup = [32,32]
-inf = [-32,-32]
 
-Otimizacao = PSO(sup,inf,{'busca':'Otimo','algoritmo':'PSO','inercia':'Constante','aceleracao':'Constante','restricao':True},30,200,w=[1.0],args_model=['ackley'])
-Otimizacao.Busca(Modelo)
-Otimizacao.Result_txt()
-Otimizacao.Movie(Nome_param=[r'$\theta_1$',r'$\theta_2$'],tipos=['projecao','funcao'])
+args_model=['ackley'] # In this example, the Modelo class needs another argument: the name of the function to be minimized
+
+# PSO Algorithm executed with decreasing inercia weight from 0.9 to 0.4; acceleration factors constant and equal to default values of 2; 30 particles and 200 iterations.
+Otimizacao = PSO(sup,inf,{'busca':'Otimo','algoritmo':'PSO','inercia':'TVIW-linear','aceleracao':'Constante','restricao':True},\
+Num_particulas=30,itmax=200,w=[0.9,0.4],args_model=args_model)
+Otimizacao.Busca(Modelo) # Do the search
+Otimizacao.Result_txt() # Printing results in the same folder as txt files
+Otimizacao.Graficos(Nome_param=[r'$\theta_1$',r'$\theta_2$'],FO2a2=True)  # Creating performance graphs in the same folder, including the objective function in 3d (FO2a2=True)
+
+# To create movie frames uncomment the line below
+# Otimizacao.Movie(Nome_param=[r'$\theta_1$',r'$\theta_2$'],tipos=['projecao','funcao']) # Creating movie frames
+
+# Printing the results:
+print Otimizacao.gbest # optimum point
+print Otimizacao.best_fitness # objective function in the optimum point
